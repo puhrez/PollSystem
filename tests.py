b623742e059591e52b922e826ba93e7b8c51989d
Michael Perez <michaelp193@gmail.com>
2015-04-01 00:04:23 -0400

    added a test todo

---
+++ tests.py
@@ -0,0 +1,26 @@
+#!flask/bin/python
+
+"""
+TODO:
+Write tests for:
+User creation
+Poll creation
+Question creation
+Token creation
+Effect creation
+
+Adding several polls to a User
+Adding several users to a Poll
+Adding several questions to a Poll
+Adding several tokens to a Poll
+Adding several effects to a question
+
+Accessing a user's polls
+Accessing a poll's users
+Accessing a poll's questions
+Acessing a question's poll
+Accessing a poll's tokens
+Accessing a token's poll
+Accessing a question's effects
+Accessing an effect's token
+"""
