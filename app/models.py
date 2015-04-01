b83599edbb6ee15e00ff3546e4cb7bbec9402df3
Michael Perez <michaelp193@gmail.com>
2015-03-31 23:41:44 -0400

    renamed tables, added relationship comments

--- app/models.py
+++ app/models.py
@@ -5,8 +5,8 @@ This is our helper table to create the many-to-many relationship
 between users and polls
 """
 memberships = db.Table('memberships',
-  db.Column('user_id', db.Integer, db.ForeignKey('users.id')),
-  db.Column('poll_id', db.Integer, db.ForeignKey('polls.id'))
+  db.Column('user_id', db.Integer, db.ForeignKey('user.id')),
+  db.Column('poll_id', db.Integer, db.ForeignKey('poll.id'))
 )
 
 class User(db.Model):
@@ -17,8 +17,9 @@ class User(db.Model):
     a password,
     a name
     and a set of polls
+  Has a many-to-many relationship with Poll
   """
-  __tablename__ = 'users'
+  __tablename__ = 'user'
   id = db.Column(db.Integer, primary_key=True)
   email = db.Column(db.String(64), index=True, unique=True)
   password = db.Column(db.String(10))
@@ -39,8 +40,11 @@ class Poll(db.Model):
     a set of questions
     a set of users (refered to from the users backref)
     a set of tokens
+  Has a many-to-many relationship with User
+  Has a one-to-many relationship with Question
+  Has a one-to-many relationship with Token
   """
-  __tablename__ = 'polls'
+  __tablename__ = 'poll'
   id = db.Column(db.Integer, primary_key=True)
   name = db.Column(db.String(65))
   timestamp = db.Column(db.DateTime)
@@ -51,50 +55,54 @@ class Poll(db.Model):
     return "<Polls %r>" % (self.name)
 
 class Question(db.Model):
-  __tablename__ = 'questions'
+  __tablename__ = 'question'
   """
   This class represents a single question which has:
     a unique id,
     a string, text, which is the question itself
     a set of effects
+  Has a one-to-many relatinoship with Effect
   """
   id = db.Column(db.Integer, primary_key=True)
   text = db.Column(db.String(250))
-  poll_id = db.Column(db.Integer, db.ForeignKey('polls.id'))
+  poll_id = db.Column(db.Integer, db.ForeignKey('poll.id'))
   effects = db.relationship('Effect', backref='question', lazy='dynamic')
 
   def __repr__(self):
     return "<Question %r>" % (self.text)
 
 class Effect(db.Model):
-  __tablename__ = 'effects'
+  __tablename__ = 'effect'
   """
   This class represents a single effect that a question has on a token:
     a unique id,
     a positive or negative value
     a token it affects
+  Has a refrence to its parent Question
+  Has a reference to its respective Token
   """
   id = db.Column(db.Integer, primary_key=True)
   value = db.Column(db.Integer)
-  question_id = db.Column(db.Integer, db.ForeignKey('questions.id'))
-  token_id = db.Column(db.Integer, db.ForeignKey('tokens.id'))
+  question_id = db.Column(db.Integer, db.ForeignKey('question.id'))
+  token_id = db.Column(db.Integer, db.ForeignKey('token.id'))
 
   def __repr__(self):
     return "<Effect %d on token-id %d>" % (self.value, self.token)
 
 class Token(db.Model):
-  __tablename__ = 'tokens'
+  __tablename__ = 'token'
   """
   This class represents a single token that has:
-  a unique id,
-  a maximum value,
-  a minimum value,
-  whether it is an upperbound
-  whether it is a lowerbound
-  a text which is the token
+    a unique id,
+    a maximum value,
+    a minimum value,
+    whether it is an upperbound
+    whether it is a lowerbound
+    a text which is the token
+  Has a reference to its parent poll
   """
   id = db.Column(db.Integer, primary_key=True)
-  poll_id = db.Column(db.Integer, db.ForeignKey('polls.id'))
+  poll_id = db.Column(db.Integer, db.ForeignKey('poll.id'))
   is_upperbound = db.Column(db.Boolean)
   is_lowerbound = db.Column(db.Boolean)
   maximum = db.Column(db.Integer)
