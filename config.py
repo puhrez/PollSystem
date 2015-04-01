8c36fec52a94b89cbb1ecd0ea42290e563031405
Michael Perez <michaelp193@gmail.com>
2015-03-31 21:27:39 -0400

    init models added

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-SQLALCHEMY_DATABASE_URI = 'postgresql://polladmin:pollMaster@localhost/pollapp'
+SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URI", 
+  'postgresql://polladmin:pollMaster@localhost/pollapp')
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
--- timesheet.md
+++ timesheet.md
@@ -1,3 +1,6 @@
 03/31/2015: 18:41 - started work on scaffolding
 03/31/2015: 18:50 - finished scaffolding and DB hooking up
-03/31/2015: 19:15 - commit and started working on models 
+03/31/2015: 19:15 - commit and started working on models in branch model
+03/31/2015: 19:30 - init models constructed
+
+ 
