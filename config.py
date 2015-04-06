92db5e62f02243dd713e085603279402c815e939
Michael Perez <michaelp193@gmail.com>
2015-04-06 14:19:36 -0400

    trying to correct travis config

--- config.py
+++ config.py
@@ -8,6 +8,7 @@ SECRET_KEY = 'fix-me'
 SQLALCHEMY_DATABASE_URI = os.environ.get(
     "DATABASE_URL",
     "postgres://lryxcvsuhgxfdz:HbyJbP2nX1JS0de8ZKBaJWC1Gl@ec2-54-163-228-58.compute-1.amazonaws.com:5432/d87i595v7efndb"
+)
 
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
 SQLALCHEMY_RECORD_QUERIES = True
