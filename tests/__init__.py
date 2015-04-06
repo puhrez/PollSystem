73be6bf362753d5a47eb947d69fe56783e3ff3fd
Michael Perez <michaelp193@gmail.com>
2015-04-06 14:23:51 -0400

    trying to correct travis config

--- tests/__init__.py
+++ tests/__init__.py
@@ -3,5 +3,5 @@ from app import app
 app.config['TESTING'] = True
 app.config['WTF_CSRF_ENABLED'] = False
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:pollMaster'
-app.config['SQLALCHEMY_DATABASE_URI'] += '@localhost/pollapptest'
+app.config['SQLALCHEMY_DATABASE_URI'] += '@localhost/system_test'
 test_app = app.test_client()
