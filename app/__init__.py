39e7c746be735a6a3051e726699b7d323e927894
Michael Perez <michaelp193@gmail.com>
2015-04-05 20:02:10 -0400

    trying to herokuize

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.restful import Api
@@ -6,5 +7,12 @@ app = Flask(__name__)
 app.config.from_object('config')
 db = SQLAlchemy(app)
 api = Api(app)
+
+if os.environ.get('HEROKU') is not None:
+    import logging
+    stream_handler = logging.StreamHandler()
+    app.logger.addHandler(stream_handler)
+    app.logger.setLevel(logging.INFO)
+    app.logger.into('poll startup')
 
 from app import views, models
--- config.py
+++ config.py
@@ -1,9 +1,16 @@
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
+CSRF_ENABLE = True
+SECRET_KEY = 'fix-me'
+
+
 SQLALCHEMY_DATABASE_URI = os.environ.get(
     "DATABASE_URL",
     'postgresql://polladmin:pollMaster@localhost/pollapp'
 )
 
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
+SQLALCHEMY_RECORD_QUERIES = True
+
+DATABASE_QUERY_TIMEOUT = 0.5
